3fa12f2bee88adb737e97957ed66a15698a95f9f
Henrique <henriquevigato@gmail.com>
2026-04-22 17:29:45 -0300

    Finalizando a L-6 do CH-1

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,53 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse the thumbnail", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get the thumbnail", err)
+		return
+	}
+	defer file.Close()
+
+	mideaType := fileHeader.Header.Get("Content-Type")
+
+	imageData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Image", err)
+		return
+	}
+
+	metaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid video id", err)
+		return
+	}
+	if metaData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User is not authorized", err)
+		return
+	}
+
+	thumbnail := thumbnail{
+		data:      imageData,
+		mediaType: mideaType,
+	}
+	videoThumbnails[videoID] = thumbnail
+	thumbnailURL := fmt.Sprintf("http://localhost:8091/api/thumbnails/%s", videoID)
+	metaData.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(metaData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Erro a atualizar o video", err)
+	}
+
+	respondWithJSON(w, http.StatusOK, metaData)
 }
