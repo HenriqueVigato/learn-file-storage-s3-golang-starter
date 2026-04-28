afc64ea8f2052bcd1643cd282fd6866fa7a4db3a
Henrique <henriquevigato@gmail.com>
2026-04-28 18:07:41 -0300

    Finalizando a L-4 do CH-2

--- app/app.js
+++ app/app.js
@@ -260,7 +260,7 @@ function viewVideo(video) {
   if (!video.thumbnail_url) {
     thumbnailImg.style.display = 'none'; } else {
     thumbnailImg.style.display = 'block';
-    thumbnailImg.src = `${video.thumbnail_url}?v=${Date.now()}`;
+    thumbnailImg.src = video.thumbnail_url;
   }
 
   const videoPlayer = document.getElementById('video-player');
